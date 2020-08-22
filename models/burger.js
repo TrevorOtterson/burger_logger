4d5fbccaceb3dc831c3632121f9e006f46a0fbcf
Trevor Otterson <totterson97@gmail.com>
2020-08-22 13:38:17 -0600

    added sever, script.js, parials, and connected all the scripts to server.js

--- models/burger.js
+++ models/burger.js
@@ -4,13 +4,13 @@ let orm = require('../config/orm.js')
 let burger = {
   all: function(cb) {
     orm.all('burgers', function(res) {
-      cb(res);
+      cb(res)
     })
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create('burgers', cols, vals, function(res) {
-      cb(res);
+      cb(res)
     })
   },
   update: function(obj_col_vals, condition, cb) {
@@ -26,4 +26,4 @@ let burger = {
 }
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burger;
+module.exports = burger
